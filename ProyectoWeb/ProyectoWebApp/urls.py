8b1dad9956a02cc0ad20f0f57d8baef4c719d3db
Jesús Apodaca <a354631@uabc.edu.mx>
2020-12-02 00:17:44 -0800

    Error de envío de parametros [RESUELTO]
    Se agregaron las URLs para las vistas con procesamiento de datos y renderizado de los documentos HTML. Validación en las vistas (try/except) que procesan datos al momento de recibir valores no deseados (-1), agregación de operador ternario para validar la presentación de los datos, presentación de redondeo para evitar grandes decimales, se resuelve el problema que provaba enviar -1 a las vistas para procesar

--- ProyectoWeb/ProyectoWebApp/urls.py
+++ ProyectoWeb/ProyectoWebApp/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='Index'),
     # name es el nombre por el cual invocas al hacer los href en los html
     # El primer argumento del path y el name puedes ser nombres diferentes, no pasa nada
+
+    # Vistas para el renderizado de los documentos sin procesamiento de información
     path('PesoChileno', views.PesoChileno, name='PesoChileno'),
     path('PesoMexicano', views.PesoMexicano, name='PesoMexicano'),
     path('PesoArgentino', views.PesoArgentino, name='PesoArgentino'),
@@ -16,4 +18,17 @@ urlpatterns = [
 
     path('Euro', views.Euro, name='Euro'),
     path('Rublo', views.Rublo, name='Rublo'),
+
+    # Vistas para el renderizado de los documentos con procesamiento de información
+    path('convertir_PesoChileno', views.convertir_PesoChileno, name='convertir_PesoChileno'),
+    path('convertir_PesoMexicano/', views.convertir_PesoMexicano, name='convertir_PesoMexicano'),
+    path('convertir_PesoArgentino', views.convertir_PesoArgentino, name='convertir_PesoArgentino'),
+    path('convertir_PesoColombiano', views.convertir_PesoColombiano, name='convertir_PesoColombiano'),
+
+    path('convertir_DolarAmericano', views.convertir_DolarAmericano, name='convertir_DolarAmericano'),
+    path('convertir_DolarCanadiense', views.convertir_DolarCanadiense, name='convertir_DolarCanadiense'),
+    path('convertir_DolarAustraliano', views.convertir_DolarAustraliano, name='convertir_DolarAustraliano'),
+
+    path('convertir_Euro', views.convertir_Euro, name='convertir_Euro'),
+    path('convertir_Rublo', views.convertir_Rublo, name='convertir_Rublo'),
 ]
--- ProyectoWeb/ProyectoWebApp/views.py
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -37,190 +37,217 @@ def Rublo(request):
 # Fecha en la que se revisaron las equivalencias de las monedas: 1 de diciembre de 2020
 # Datos proporcionados por Google
 def convertir_PesoMexicano(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = int(request.GET["tbCantidad"]) # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
 
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 0.068
-    _dConversion['Dolar Americano'] = ctd * 0.050
-    _dConversion['Dolar Canadiense'] = ctd * 0.065
-    _dConversion['Peso argetino'] = ctd * 4.07
-    _dConversion['Peso chileno'] = ctd * 37.94
-    _dConversion['Peso colombiano'] = ctd * 177.15
-    _dConversion['Peso mexicano'] = ctd
-    _dConversion['Euro'] = ctd * 0.041
-    _dConversion['Rublo ruso'] = ctd * 3.79
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 0.068, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 0.050, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 0.065, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 4.07, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 37.94, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 177.15, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.041, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round( ctd * 3.79, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/PesoMexicano.html', _dContenedor)
 
 def convertir_PesoColombiano(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = request.GET["tbCantidad"] # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 0.00038
-    _dConversion['Dolar Americano'] = ctd * 0.00028
-    _dConversion['Dolar Canadiense'] = ctd * 0.00036
-    _dConversion['Peso argetino'] = ctd * 0.023
-    _dConversion['Peso chileno'] = ctd * 0.21
-    _dConversion['Peso colombiano'] = ctd
-    _dConversion['Peso mexicano'] = ctd * 0.0056
-    _dConversion['Euro'] = ctd * 0.00023
-    _dConversion['Rublo ruso'] = ctd * 0.021
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 0.00038, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 0.00028, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 0.00036, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 0.023, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 0.21, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 0.0056, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.00023, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd * 0.021, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/PesoColombiano.html')
 
 def convertir_PesoChileno(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = request.GET["tbCantidad"] # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 0.0018
-    _dConversion['Dolar Americano'] = ctd * 0.0013
-    _dConversion['Dolar Canadiense'] = ctd * 0.0017
-    _dConversion['Peso argetino'] = ctd * 0.11
-    _dConversion['Peso chileno'] = ctd
-    _dConversion['Peso colombiano'] = ctd * 4.67
-    _dConversion['Peso mexicano'] = ctd * 0.026
-    _dConversion['Euro'] = ctd * 0.0011
-    _dConversion['Rublo ruso'] = ctd * 0.1
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 0.0018, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 0.0013, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 0.0017, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 0.11, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 4.67, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 0.026, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.0011, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd * 0.1, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/PesoChileno.html')
 
 def convertir_PesoArgentino(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = request.GET["tbCantidad"] # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 0.0017
-    _dConversion['Dolar Americano'] = ctd * 0.012
-    _dConversion['Dolar Canadiense'] = ctd * 0.016
-    _dConversion['Peso argetino'] = ctd
-    _dConversion['Peso chileno'] = ctd * 9.33
-    _dConversion['Peso colombiano'] = ctd * 43.57
-    _dConversion['Peso mexicano'] = ctd * 0.25
-    _dConversion['Euro'] = ctd * 0.01
-    _dConversion['Rublo ruso'] = ctd * 0.93
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 0.0017, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 0.012, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 0.016, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 9.33, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 43.57, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 0.25, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.01, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd * 0.93, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/PesoArgentino.html')
 
 def convertir_DolarAmericano(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = request.GET["tbCantidad"] # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 1.36
-    _dConversion['Dolar Americano'] = ctd
-    _dConversion['Dolar Canadiense'] = ctd * 1.29
-    _dConversion['Peso argetino'] = ctd * 81.44
-    _dConversion['Peso chileno'] = ctd * 760
-    _dConversion['Peso colombiano'] = ctd * 3548.5
-    _dConversion['Peso mexicano'] = ctd * 20.03
-    _dConversion['Euro'] = ctd * 0.83
-    _dConversion['Rublo ruso'] = ctd * 75.83
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 1.36, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 1.29, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 81.44, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 760, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 3548.5, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 20.03, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.83, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd * 75.83, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/DolarAmericano.html')
 
 def convertir_DolarCanadiense(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = request.GET["tbCantidad"] # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 1.05
-    _dConversion['Dolar Americano'] = ctd * 0.77
-    _dConversion['Dolar Canadiense'] = ctd
-    _dConversion['Peso argetino'] = ctd * 62.91
-    _dConversion['Peso chileno'] = ctd * 587.13
-    _dConversion['Peso colombiano'] = ctd * 2741.36
-    _dConversion['Peso mexicano'] = ctd * 15.48
-    _dConversion['Euro'] = ctd * 0.64
-    _dConversion['Rublo ruso'] = ctd * 58.61
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 1.05, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 0.77, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 62.91, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 587.13, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 2741.36, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 15.48, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.64, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd * 58.61, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/DolarCanadiense.html')
 
 def convertir_DolarAustraliano(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = request.GET["tbCantidad"] # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd
-    _dConversion['Dolar Americano'] = ctd * 0.074
-    _dConversion['Dolar Canadiense'] = ctd * 0.95
-    _dConversion['Peso argetino'] = ctd * 60
-    _dConversion['Peso chileno'] = ctd * 560.1
-    _dConversion['Peso colombiano'] = ctd * 2614.57
-    _dConversion['Peso mexicano'] = ctd * 14.76
-    _dConversion['Euro'] = ctd * 0.61
-    _dConversion['Rublo ruso'] = ctd * 55.9
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 0.074, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 0.95, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 60, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 560.1, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 2614.57, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 14.76, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.61, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd * 55.9, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/DolarAustraliano.html')
 
 def convertir_Euro(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = request.GET["tbCantidad"] # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 1.64
-    _dConversion['Dolar Americano'] = ctd * 1.21
-    _dConversion['Dolar Canadiense'] = ctd * 1.56
-    _dConversion['Peso argetino'] = ctd * 98.26
-    _dConversion['Peso chileno'] = ctd * 917.05
-    _dConversion['Peso colombiano'] = ctd * 4281.76
-    _dConversion['Peso mexicano'] = ctd * 24.17
-    _dConversion['Euro'] = ctd
-    _dConversion['Rublo ruso'] = ctd * 91.53
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 1.64, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 1.21, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 1.56, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 98.26, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 917.05, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 4281.76, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 24.17, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd * 91.53, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/Euro.html')
 
 def convertir_Rublo(request):
-    ctd = int(request.POST.get("tbCantidad")) # obtener cantidad recibida por POST 
+    try:
+        ctd = int(request.GET["tbCantidad"]) # obtener cantidad recibida por GET
+    except KeyError:
+        ctd = -1
     
     # El diccionario contenedor incluye una clave cuyo valor será el diccionario 
     # que será lleno de las conversiones, esto, para realizar el for each 
     _dContenedor, _dConversion = {}, {} # Diccionarios para el envio de valores
 
     # Manipulación de los datos
-    _dConversion['Dolar Australiano'] = ctd * 0.018
-    _dConversion['Dolar Americano'] = ctd * 0.013
-    _dConversion['Dolar Canadiense'] = ctd * 0.017
-    _dConversion['Peso argetino'] = ctd * 1.07
-    _dConversion['Peso chileno'] = ctd * 10.02
-    _dConversion['Peso colombiano'] = ctd * 46.77
-    _dConversion['Peso mexicano'] = ctd * 0.26
-    _dConversion['Euro'] = ctd * 0.011
-    _dConversion['Rublo ruso'] = ctd
+    _dConversion['Dolar Australiano'] = 0 if ctd == -1 else round(ctd * 0.018, 3)
+    _dConversion['Dolar Americano'] = 0 if ctd == -1 else round(ctd * 0.013, 3)
+    _dConversion['Dolar Canadiense'] = 0 if ctd == -1 else round(ctd * 0.017, 3)
+    _dConversion['Peso argetino'] = 0 if ctd == -1 else round(ctd * 1.07, 3)
+    _dConversion['Peso chileno'] = 0 if ctd == -1 else round(ctd * 10.02, 3)
+    _dConversion['Peso colombiano'] = 0 if ctd == -1 else round(ctd * 46.77, 3)
+    _dConversion['Peso mexicano'] = 0 if ctd == -1 else round(ctd * 0.26, 3)
+    _dConversion['Euro'] = 0 if ctd == -1 else round(ctd * 0.011, 3)
+    _dConversion['Rublo ruso'] = 0 if ctd == -1 else round(ctd, 3)
 
     _dContenedor['conversiones'] = _dConversion
     return render(request, 'ProyectoWebApp/Rublo.html')
